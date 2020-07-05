29ab5a18fbc18f563b142bb4090463ba9182ddc2
Saska <krcicaleksandra@gmail.com>
2020-07-06 01:17:45 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApplicationListComponent } from './components/application-list/application-list.component';
+import { ApplicationComponent } from './components/application/application.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApplicationListComponent,
+    ApplicationComponent
   ],
   imports: [
     BrowserModule,
